da5e162bca34002b87655fccfba1db177ef82647
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2018-02-19 05:04:27 +0900

    Refactor

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,8 +1,9 @@
+use std::env::Args;
 use std::env::args;
 use std::string::String;
 
 fn main() {
-    for i in 1..max() {
+    for i in 1..max(args()) {
         let mut s = String::from("");
 
         if i % 3 == 0 {
@@ -19,15 +20,11 @@ fn main() {
     }
 }
 
-fn max() -> u32 {
-    let args: Vec<_> = args().collect();
+const DEFAULT_MAX: u32 = 101;
 
-    if args.len() > 1 {
-        match args[1].parse::<u32>() {
-            Ok(n) => return n + 1,
-            Err(_) => return 101,
-        };
-    } else {
-        return 101;
-    }
+fn max(args: Args) -> u32 {
+    let mut args = args;
+    return args.nth(1)
+        .map(|s| s.parse().unwrap_or(DEFAULT_MAX))
+        .unwrap_or(DEFAULT_MAX);
 }
